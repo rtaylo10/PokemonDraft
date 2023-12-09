59a8f02d3982e4fa772fee0fdf573501c60cdf85
rtaylo10 <ryantaylor5050@yahoo.ca>
2023-12-09 04:15:46 -0500

    Setup for github pages

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: "/pokemon-draft-picks/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
