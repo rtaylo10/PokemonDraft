b59090ad4985c02d6b6f90c4d573dfdcad887749
rtaylo10 <ryantaylor5050@yahoo.ca>
2023-12-09 04:16:33 -0500

    update base url

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/pokemon-draft-picks/",
+  base: "/PokemonDraft/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
